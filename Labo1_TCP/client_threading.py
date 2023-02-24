8559473dc75f286ac1d9443af12f8f635d6b9c1b
De-Melk <belmansyorrit@gmail.com>
2023-02-24 19:31:20 +0100

    client and server threading files

---
+++ Labo1_TCP/client_threading.py
@@ -0,0 +1,31 @@
+import threading
+import socket
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+DISCONNECT_MESSAGE = "!DISCONECT"
+connected = True
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+print("[Connected to server]")
+
+def recv_msg():
+    global connected
+    while connected:
+        recv_msg = s.recv(1024)
+        print("[Server] ->",recv_msg.decode('UTF-8'))
+
+def send_msg():
+    global connected
+    while connected:
+        send_msg = input(str())
+        s.send(send_msg.encode('UTF-8'))
+        if send_msg == DISCONNECT_MESSAGE:
+            connected = False
+
+t = threading.Thread(target=recv_msg)
+t.start()
+
+send_msg()
---
+++ Labo1_TCP/server_threading.py
@@ -0,0 +1,49 @@
+import threading
+import socket
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+DISCONNECT_MESSAGE = "!DISCONECT"
+ADDR = (HOST, PORT)
+connected = False
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(ADDR)
+
+def recv_msg(conn, addr):
+    global connected
+    while connected:
+        recv_msg = conn.recv(1024)
+        print(f"[{addr}] ->", recv_msg.decode('UTF-8'))
+        if recv_msg.decode('UTF-8') == DISCONNECT_MESSAGE:
+            conn.sendall("[Disconected]".encode('UTF-8'))
+            conn.close()
+            connected = False
+            print(f"[{addr}] ->", "CLOSED THE CONNECTION!")
+
+def send_msg(conn, addr):
+    global connected
+    while connected:
+        try:
+            send_msg = input(str())
+            conn.sendall(send_msg.encode('UTF-8'))
+        except:
+            print("[No Client] can't send message when no client is connected")
+
+def start():
+    global connected
+    while not connected:
+        conn, addr = s.accept()
+        print(f"[Connected] client {addr} connected")
+        connected = True
+        t_recv = threading.Thread(target=recv_msg, args=(conn, addr))
+        t_recv.start()
+        t_send = threading.Thread(target=send_msg, args=(conn, addr))
+        t_send.start()
+
+print("[STARTING] server is starting...")
+s.listen()
+print(f"[LISTENING] Server is listening on {HOST}")
+
+while True:
+    start()
