3c27e305d9eda934cf41449f299383b8238cc5bc
De-Melk <belmansyorrit@gmail.com>
2023-03-09 08:57:50 +0100

    Server FileSharing

--- Labo2_TCP_Uitbreiding/Task1_server.py
+++ Labo2_TCP_Uitbreiding/Task1_server.py
@@ -5,7 +5,9 @@ import socket
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 DISCONNECT_MESSAGE = "!DISCONECT"
+FILESHARE_MESSAGE = "!FILE"
 ADDR = (HOST, PORT)
+SIZE = 1024
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(ADDR)
@@ -20,9 +22,23 @@ def handle_client(conn, addr):
         if data.decode('UTF-8') == DISCONNECT_MESSAGE:
             connected = False
             response = "[Disconected]".encode('UTF-8')
-        else:
+        elif data.decode('UTF-8') == FILESHARE_MESSAGE:
+            """ Receiving the filename from the client. """
+            filename = conn.recv(SIZE).decode('UTF-8')
+            print(f"[RECV] Receiving the filename.")
+            file = open(filename, "w")
+            conn.send("Filename received.".encode('UTF-8'))
+            """ Receiving the file data from the client. """
+            data = conn.recv(SIZE).decode('UTF-8')
+            print(f"[RECV] Receiving the file data.")
+            file.write(data)
+            conn.send("File data received".encode('UTF-8'))
+            else:
             print(f"[{addr}] ->", data.decode('UTF-8'))
             response = "[MessageReceived]".encode('UTF-8')
+
+            connected = False
+            response = "[Disconected]".encode('UTF-8')
 
         conn.sendall(response)
     
